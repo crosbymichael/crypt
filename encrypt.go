14714cac17879275369e855dfc9035555e0c2560
Michael Crosby <crosbymichael@gmail.com>
2014-11-22 19:14:27 -0800

    Implement encrypt command

---
+++ encrypt.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"io"
+	"os"
+
+	"github.com/codegangsta/cli"
+)
+
+var encryptCommand = cli.Command{
+	Name:  "encrypt",
+	Usage: "encript a file",
+	Flags: []cli.Flag{
+		cli.StringFlag{Name: "key", Usage: "key to use for the encryption algo"},
+	},
+	Action: encryptAction,
+}
+
+func getKey(context *cli.Context) string {
+	return context.String("key")
+}
+
+func encryptFile(in, out string, key []byte) error {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return err
+	}
+	of, err := os.Create(out)
+	if err != nil {
+		return err
+	}
+	defer of.Close()
+	var iv [aes.BlockSize]byte
+	w := &cipher.StreamWriter{S: cipher.NewOFB(block, iv[:]), W: of}
+
+	f, err := os.Open(in)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	if _, err := io.Copy(w, f); err != nil {
+		return err
+	}
+	return nil
+}
+
+func encryptAction(context *cli.Context) {
+	if len(context.Args()) != 2 {
+		logger.Fatal("invalid number of arguments: <file in> <file out>")
+	}
+	key := getKey(context)
+	if key == "" {
+		logger.Fatal("no key provided")
+	}
+	if err := encryptFile(context.Args().Get(0), context.Args().Get(1), []byte(key)); err != nil {
+		logger.Fatal(err)
+	}
+}
---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"os"
+
+	"github.com/Sirupsen/logrus"
+	"github.com/codegangsta/cli"
+)
+
+var logger = logrus.New()
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "crypt"
+	app.Version = "1"
+	app.Author = "@crosbymichael"
+	app.Commands = []cli.Command{
+		encryptCommand,
+		//		decryptCommand,
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		logger.Fatal(err)
+	}
+}
