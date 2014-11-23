9300b5fe5d0f47d0da7ef31612af333222e02ee0
Michael Crosby <crosbymichael@gmail.com>
2014-11-22 21:20:44 -0800

    Update readme with examples

--- README.md
+++ README.md
@@ -8,6 +8,44 @@ A key can be specified with the `--key` flag or via stdin if `--key` is
 not specified.  You may want to provide your key via stdin for encryption
 and decryption so that it is not saved in your bash history.
 
+### Examples
+
+**Encrypt the contents from STDIN to a file**
+```bash
+> echo "hello world" | crypt -e --key test -i encrypted-text
+```
+
+**Decrypt the contents from STDIN to STDOUT**
+```bash
+> cat encrypted-text | crypt -d --key test -i -o
+hello world
+```
+
+**Encrypt a large tar file to a new file**
+```bash
+> crypt -e docker-image.tar encrypted-image.xxx
+please enter your key:
+> secret
+1.24 GB / 1.25 GB [=========================================================================] 99.70 % 58.72 MB/s
+# test to see if I can read the file
+> tar -tvf encrypted-image.xxx
+tar: This does not look like a tar archive
+tar: Skipping to next header
+tar: Exiting with failure status due to previous errors
+```
+
+**Decrypt a large file back to the original contents**
+```bash
+> crypt -d encrypted-image.xxx docker-image-unencrypted.tar
+please enter your key:
+> secret
+1.24 GB / 1.25 GB [=========================================================================] 99.45 % 70.80 MB/s
+# look at the tar headers
+> tar -tvf docker-image-unencrypted.tar
+drwxr-xr-x 0/0               0 2014-11-22 21:14 05c6d847812ccad7f327f12ec2404dd972fc64e65c6c8a996b402c8e3f990d7c/
+-rw-r--r-- 0/0               3 2014-11-22 21:14 05c6d847812ccad7f327f12ec2404dd972fc64e65c6c8a996b402c8e3f990d7c/
+VERSION
+```
 
 ```bash
 NAME:
@@ -29,9 +67,13 @@ GLOBAL OPTIONS:
    --key                key to use for the encryption algo
    --encrypt, -e        encrypt a file
    --decrypt, -d        decrypt a file
+   --stdin, -i          accept input for STDIN
+   --stdout, -o         return output to STDOUT
    --help, -h           show help
    --version, -v        print the version
 ```
 
+### TODO:
+* Keep it simple stupid
 
 ## License - MIT
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"crypto/cipher"
+	"fmt"
 	"io"
 	"os"
 
@@ -19,30 +20,49 @@ func decrypt(in, out *os.File, stream cipher.Stream) (io.Reader, io.Writer) {
 	return &cipher.StreamReader{S: stream, R: in}, out
 }
 
+func getInputFile(context *cli.Context) *os.File {
+	if context.GlobalBool("stdin") {
+		return os.Stdin
+	}
+	in, err := os.Open(context.Args().Get(0))
+	if err != nil {
+		logger.Fatal(err)
+	}
+	return in
+}
+
+func stdoutArgIndex(context *cli.Context) int {
+	if !context.GlobalBool("stdin") {
+		return 1
+	}
+	return 0
+}
+
+func getOutputFile(context *cli.Context) *os.File {
+	if context.GlobalBool("stdout") {
+		return os.Stdout
+	}
+	out, err := os.Create(context.Args().Get(stdoutArgIndex(context)))
+	if err != nil {
+		logger.Fatal(err)
+	}
+	return out
+}
+
 func action(wrap rwHandler) func(*cli.Context) {
 	return func(context *cli.Context) {
-		if len(context.Args()) != 2 {
-			logger.Fatal("invalid number of arguments: <file in> <file out>")
-		}
 		key := getKey(context)
 		if len(key) == 0 {
 			logger.Fatal("no key provided")
 		}
-		in, err := os.Open(context.Args().Get(0))
+		in, out := getInputFile(context), getOutputFile(context)
+		err := process(in, out, key, wrap)
+		in.Close()
+		out.Close()
 		if err != nil {
-			logger.Fatal(err)
-		}
-		defer in.Close()
-		out, err := os.Create(context.Args().Get(1))
-		if err != nil {
-			in.Close()
-			logger.Fatal(err)
-		}
-		defer out.Close()
-		if err := process(in, out, key, wrap); err != nil {
-			in.Close()
-			out.Close()
-			os.Remove(context.Args().Get(1))
+			if outPath := context.Args().Get(stdoutArgIndex(context)); outPath != "" {
+				os.Remove(outPath)
+			}
 			logger.Fatal(err)
 		}
 	}
@@ -58,8 +78,13 @@ func main() {
 		cli.StringFlag{Name: "key", Usage: "key to use for the encryption algo"},
 		cli.BoolFlag{Name: "encrypt,e", Usage: "encrypt a file"},
 		cli.BoolFlag{Name: "decrypt,d", Usage: "decrypt a file"},
+		cli.BoolFlag{Name: "stdin,i", Usage: "accept input for STDIN"},
+		cli.BoolFlag{Name: "stdout,o", Usage: "return output to STDOUT"},
 	}
 	app.Before = func(context *cli.Context) error {
+		if context.GlobalBool("stdin") && context.GlobalString("key") == "" {
+			return fmt.Errorf("--key must be supplied when receiving input via STDIN")
+		}
 		switch {
 		case context.GlobalBool("encrypt"):
 			app.Action = action(encrypt)
