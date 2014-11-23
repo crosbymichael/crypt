703b34dda55640bcbd4846b9c384aaa11e87799e
Michael Crosby <crosbymichael@gmail.com>
2014-11-22 20:07:41 -0800

    Refactor into handlers

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"crypto/cipher"
+	"io"
 	"os"
 
 	"github.com/Sirupsen/logrus"
@@ -9,14 +11,33 @@ import (
 
 var logger = logrus.New()
 
+func encrypt(in, out *os.File, stream cipher.Stream) (io.Reader, io.Writer) {
+	return in, &cipher.StreamWriter{S: stream, W: out}
+}
+
+func decrypt(in, out *os.File, stream cipher.Stream) (io.Reader, io.Writer) {
+	return &cipher.StreamReader{S: stream, R: in}, out
+}
+
 func main() {
 	app := cli.NewApp()
 	app.Name = "crypt"
 	app.Version = "1"
 	app.Author = "@crosbymichael"
+	app.Flags = []cli.Flag{
+		cli.StringFlag{Name: "key", Usage: "key to use for the encryption algo"},
+	}
 	app.Commands = []cli.Command{
-		encryptCommand,
-		decryptCommand,
+		cli.Command{
+			Name:   "encrypt",
+			Usage:  "encript a file",
+			Action: handler(encrypt),
+		},
+		cli.Command{
+			Name:   "decrypt",
+			Usage:  "decrypt a file",
+			Action: handler(decrypt),
+		},
 	}
 
 	if err := app.Run(os.Args); err != nil {
--- utils.go
+++ utils.go
@@ -1,13 +1,68 @@
 package main
 
 import (
+	"crypto/aes"
+	"crypto/cipher"
 	"crypto/md5"
+	"io"
+	"os"
 
 	"github.com/codegangsta/cli"
+	"github.com/rakyll/pb"
 )
 
+type rwHandler func(in, out *os.File, stream cipher.Stream) (io.Reader, io.Writer)
+
+func process(in, out string, key []byte, h rwHandler) error {
+	outf, err := os.Create(out)
+	if err != nil {
+		return err
+	}
+	defer outf.Close()
+
+	inf, err := os.Open(in)
+	if err != nil {
+		return err
+	}
+	defer inf.Close()
+
+	bar, err := newProgressBar(inf)
+	if err != nil {
+		return err
+	}
+
+	var iv [aes.BlockSize]byte
+	block, err := newBlock(key)
+	if err != nil {
+		return err
+	}
+	stream := cipher.NewOFB(block, iv[:])
+	r, w := h(inf, outf, stream)
+	mw := io.MultiWriter(w, bar)
+	if _, err := io.Copy(mw, r); err != nil {
+		return err
+	}
+	return nil
+}
+
+func newProgressBar(in *os.File) (io.Writer, error) {
+	stat, err := in.Stat()
+	if err != nil {
+		return nil, err
+	}
+	bar := pb.New(int(stat.Size())).SetUnits(pb.U_BYTES)
+	bar.ShowSpeed = true
+	bar.ShowTimeLeft = false
+	bar.Start()
+	return bar, nil
+}
+
+func newBlock(key []byte) (cipher.Block, error) {
+	return aes.NewCipher(hashKey(key))
+}
+
 func getKey(context *cli.Context) string {
-	return context.String("key")
+	return context.GlobalString("key")
 }
 
 func hashKey(key []byte) []byte {
@@ -18,7 +73,7 @@ func hashKey(key []byte) []byte {
 	return h.Sum(nil)
 }
 
-func handler(wrap func(string, string, []byte) error) func(*cli.Context) {
+func handler(wrap rwHandler) func(*cli.Context) {
 	return func(context *cli.Context) {
 		if len(context.Args()) != 2 {
 			logger.Fatal("invalid number of arguments: <file in> <file out>")
@@ -27,7 +82,7 @@ func handler(wrap func(string, string, []byte) error) func(*cli.Context) {
 		if key == "" {
 			logger.Fatal("no key provided")
 		}
-		if err := wrap(context.Args().Get(0), context.Args().Get(1), []byte(key)); err != nil {
+		if err := process(context.Args().Get(0), context.Args().Get(1), []byte(key), wrap); err != nil {
 			logger.Fatal(err)
 		}
 	}
