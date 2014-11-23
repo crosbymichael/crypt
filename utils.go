74910ffeaadb91315ff7e06b3466d9a7253516a3
Michael Crosby <crosbymichael@gmail.com>
2014-11-22 20:50:19 -0800

    Don't use progress bar on 0 size

--- utils.go
+++ utils.go
@@ -22,23 +22,26 @@ func process(in, out *os.File, key []byte, h rwHandler) error {
 		return err
 	}
 	r, w := h(in, out, cipher.NewOFB(block, iv[:]))
-	bar, err := newProgressBar(in)
+	o, err := newProgressBar(in, w)
 	if err != nil {
 		return err
 	}
-	_, err = io.Copy(io.MultiWriter(w, bar), r)
+	_, err = io.Copy(o, r)
 	return err
 }
 
-func newProgressBar(in *os.File) (io.Writer, error) {
+func newProgressBar(in *os.File, out io.Writer) (io.Writer, error) {
 	stat, err := in.Stat()
 	if err != nil {
 		return nil, err
 	}
+	if stat.Size() == 0 {
+		return out, nil
+	}
 	bar := pb.New(int(stat.Size())).SetUnits(pb.U_BYTES)
 	bar.ShowSpeed, bar.ShowTimeLeft = true, false
 	bar.Start()
-	return bar, nil
+	return io.MultiWriter(out, bar), nil
 }
 
 func getKey(context *cli.Context) []byte {
